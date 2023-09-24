388b83ceea48fa947815b23dfca85f7a782ba081
kotofuka <hollidaymars@gmail.com>
2023-09-24 17:30:30 +0300

    date_base

--- Lavaro/Lavaro/settings.py
+++ Lavaro/Lavaro/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Lavaro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lavaroDB',                      
+        'USER': 'postgres',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
