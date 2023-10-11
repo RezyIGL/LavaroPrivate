3ed055c7ec62b052895c4f0b34643df7be0fb8a5
Rezy <64067988+RezyIGL@users.noreply.github.com>
2023-10-11 16:50:17 +0300

    fixed chats url and temp reg bttn

--- Lavaro/LavaroWeb/urls.py
+++ Lavaro/LavaroWeb/urls.py
@@ -3,14 +3,9 @@ from django.urls import path, re_path
 from . import views
 
 app_name = "LavaroWeb"
-urlpatterns = [
-    # path("", views.IndexView.as_view(), name="index"),
-    # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
-    
-    path("Profile", views.profile, name="profilePage"),
+urlpatterns = [    
     path("", views.home, name="welcomePage"),
+    path("Profile", views.profile, name="profilePage"),
     path("Chats", views.chat, name="chats"),
     path("Vacancy/<int:vacancy_id>", views.vacancy)
 ]
