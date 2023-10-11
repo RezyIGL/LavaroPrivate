24b1410e2f8eeacc730c36fb09b2c1e0f0390701
kotofuka <hollidaymars@gmail.com>
2023-10-11 10:26:54 +0300

    fix

--- LavaroWeb/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "LavaroWeb"
-urlpatterns = [
-    path("Profile/", views.profile),
-    path("", views.home),
-    path("Chats/", views.chat, name="chats")
-]
