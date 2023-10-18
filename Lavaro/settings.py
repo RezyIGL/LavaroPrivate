d8d409f0f436792ca9c81cb69c62f34286993ef0
kotofuka <hollidaymars@gmail.com>
2023-10-18 09:16:36 +0300

    normalize

--- Lavaro/settings.py
+++ Lavaro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kl6(1oq-u#g5_5(l#xvsa7r0#wf0d7%evi6(n4e0d-@$diq3%p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,16 +79,15 @@ WSGI_APPLICATION = 'Lavaro.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'LavaroDB',                      
-        'USER': 'postgres',
-        'PASSWORD': '1111',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('PGNAME'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASS'),
+        'HOST': config('PGHOST'),
+        'PORT': config('PGPORT'),
+        },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
