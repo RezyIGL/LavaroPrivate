04c42d4f2f3a29d22594b6d0697c39a2190f3379
Rezy <onlyrezystor@mail.ru>
2023-10-25 08:37:27 +0300

    little fix
    little trouble fixed

--- LavaroWeb/urls.py
+++ LavaroWeb/urls.py
@@ -7,7 +7,7 @@ app_name = "LavaroWeb"
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('signup', SignUpView.as_view(), name='signup'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path("Vacancy", views.vacancy_list, name="vacancy_list"),
     path("Profile/<int:user_id>", views.profile_detail, name="profile_detail"), 
     path("Chats", views.chat, name="chats"),
