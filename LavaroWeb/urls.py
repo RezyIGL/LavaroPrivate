1615b0b3d715685fda53b5d06e86c6582d02461c
Rezy <onlyrezystor@mail.ru>
2023-11-09 09:43:03 +0300

    Update urls.py

--- LavaroWeb/urls.py
+++ LavaroWeb/urls.py
@@ -7,7 +7,7 @@ app_name = "LavaroWeb"
 
 urlpatterns = [
     path('', RedirectView.as_view(url="/login/"), name='index'),
-    path('/login/', TemplateView.as_view(template_name='registration/login.html'), name='home'),
+    path('login/', TemplateView.as_view(template_name='registration/login.html'), name='home'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path("Vacancy", views.vacancy_list, name="vacancy_list"),
     path("Profile/<int:user_id>", views.profile_detail, name="profile_detail"), 
